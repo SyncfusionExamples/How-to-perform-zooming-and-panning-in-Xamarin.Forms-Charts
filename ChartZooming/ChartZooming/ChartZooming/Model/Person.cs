0fbf956aa51204ed5ead4202728d0df4e220664d
Hemalatha Marikumar <hemalatha.marikumar@syncfusion.com>
2021-02-20 20:38:08 +0530

    Chart zooming and panning sample has been included

---
+++ ChartZooming/ChartZooming/ChartZooming/Model/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChartZooming.Model
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public double Height { get; set; }
+
+        public Person(string name, double height)
+        {
+            this.Name = name;
+            this.Height = height;
+        }
+    }
+}
---
+++ ChartZooming/ChartZooming/ChartZooming/ViewModel/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using ChartZooming.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace ChartZooming
+{
+    public class ViewModel
+    {
+        public ObservableCollection<ExportChartDataModel> ClimateData { get; set; }
+        public ViewModel()
+        {
+            ClimateData = new ObservableCollection<ExportChartDataModel>();
+            ClimateData.Add(new ExportChartDataModel() { Month = "Jan", Temperature = 33 });
+            ClimateData.Add(new ExportChartDataModel() { Month = "Feb", Temperature = 37 });
+            ClimateData.Add(new ExportChartDataModel() { Month = "Mar", Temperature = 39 });
+            ClimateData.Add(new ExportChartDataModel() { Month = "Apr", Temperature = 43 });
+            ClimateData.Add(new ExportChartDataModel() { Month = "May", Temperature = 45 });
+            ClimateData.Add(new ExportChartDataModel() { Month = "Jun", Temperature = 43 });
+            ClimateData.Add(new ExportChartDataModel() { Month = "Jul", Temperature = 41 });
+            ClimateData.Add(new ExportChartDataModel() { Month = "Aug", Temperature = 40 });
+            ClimateData.Add(new ExportChartDataModel() { Month = "Sep", Temperature = 39 });
+            ClimateData.Add(new ExportChartDataModel() { Month = "Oct", Temperature = 39 });
+            ClimateData.Add(new ExportChartDataModel() { Month = "Nov", Temperature = 34 });
+            ClimateData.Add(new ExportChartDataModel() { Month = "Dec", Temperature = 33 });
+        }
+    }
+
+    public class ExportChartDataModel
+    {
+        public string Month { get; set; }
+        public double Temperature { get; set; }
+    }
+}
